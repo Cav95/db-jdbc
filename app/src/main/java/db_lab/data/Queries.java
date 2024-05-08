10db5108b755900ef791b9722b27c58b5eddbd5e
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-05-09 00:32:46 +0200

    Remove solutions

--- app/src/main/java/db_lab/data/Queries.java
+++ app/src/main/java/db_lab/data/Queries.java
@@ -4,29 +4,17 @@ public final class Queries {
 
     public static final String TAGS_FOR_PRODUCT =
         """
-        select tag_name
-        from   TAGGED
-        where  product_code = ?
         """;
 
     public static final String LIST_PRODUCTS =
         """
-        select code, name
-        from   PRODUCT
         """;
 
     public static final String PRODUCT_COMPOSITION =
         """
-        select MATERIAL.code, MATERIAL.description, COMPOSITION.percent
-        from   COMPOSITION, MATERIAL
-        where  COMPOSITION.material_code = MATERIAL.code
-        and    COMPOSITION.product_code = ?
         """;
 
     public static final String FIND_PRODUCT =
         """
-        select code, name, description
-        from   PRODUCT
-        where  code = ?
         """;
 }
